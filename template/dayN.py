4c41fcf2f040dae392f5b716d297ed87c9fb819f
Kyle Benesch <4b796c65+github@gmail.com>
2021-12-11 23:08:25 -0800

    Update template.

--- template/dayN.py
+++ template/dayN.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import aocd  # type: ignore
 import numpy as np
 
-THIS_DIR = Path(__file__).parent
-
 
 def main(file: Path) -> (int | str):
     with open(file, "r", encoding="utf8") as f:
@@ -18,5 +16,12 @@ def main(file: Path) -> (int | str):
 
 EXPECTED = -1
 if __name__ == "__main__":
-    if main(THIS_DIR / "example.txt") == EXPECTED:
-        aocd.submit(main(THIS_DIR / "input.txt"))
+    THIS_DIR = Path(__file__).parent
+    EXAMPLE_FILE = THIS_DIR / "example.txt"
+    INPUT_FILE = THIS_DIR / "input.txt"
+    if not INPUT_FILE.exists():
+        data = aocd.get_data() + "\n"
+        with open(INPUT_FILE, "w", encoding="utf8") as f:
+            f.write(data)
+    if main(EXAMPLE_FILE) == EXPECTED:
+        aocd.submit(main(INPUT_FILE))
