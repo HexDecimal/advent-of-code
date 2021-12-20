5cb49f6e702e9204e47f52d61a1b06ea12994c73
Kyle Benesch <4b796c65+github@gmail.com>
2021-12-19 21:33:34 -0800

    Day 20.

---
+++ 20/day20.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
+import scipy.signal
+
+CONVOLE_BITS = np.asarray(
+    [
+        [256, 128, 64],
+        [32, 16, 8],
+        [4, 2, 1],
+    ]
+)[::-1, ::-1]
+
+
+def main(file: Path) -> (int | str):
+    with open(file, "r", encoding="utf8") as f:
+        (enhancement_, image_) = f.read().strip().split("\n\n")
+    enhancement = np.asarray([c == "#" for c in enhancement_], bool)
+    image = np.asarray([[c == "#" for c in row] for row in image_.split("\n")])
+    border = False
+
+    for _ in range(50):
+        image_index = scipy.signal.convolve2d(
+            np.pad(image, pad_width=((2, 2), (2, 2)), constant_values=border), CONVOLE_BITS, mode="valid"
+        )
+        image = enhancement[image_index]
+        if enhancement[0]:
+            border = not border
+
+    print(image.sum())
+    return image.sum()
+
+
+EXPECTED = 3351
+if __name__ == "__main__":
+    THIS_DIR = Path(__file__).parent
+    EXAMPLE_FILE = THIS_DIR / "example.txt"
+    INPUT_FILE = THIS_DIR / "input.txt"
+    if main(EXAMPLE_FILE) == EXPECTED:
+        main(INPUT_FILE)
