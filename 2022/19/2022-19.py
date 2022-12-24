94ff23803eb61dfb23f5877889fe3f04839d347d
Kyle Benesch <4b796c65+github@gmail.com>
2022-12-24 00:29:16 -0800

    2022-19

--- 2022/19/2022-19.py
+++ 2022/19/2022-19.py
@@ -1,19 +1,13 @@
 from __future__ import annotations
 
-import functools
-import itertools
-import operator
-import re
 from pathlib import Path
 from typing import *  # noqa: F403
 
 import aocd  # type: ignore
-import numpy as np
-from aoc import as_array, as_bool_array, as_ord_array, ints, reduce_multiply, split_ints
-from numpy.typing import NDArray
-from parse import parse
+from aoc import ints, reduce_multiply
 import attrs
 import multiprocessing.pool
+import heapq
 
 ORE = 0
 CLAY = 1
@@ -43,95 +37,141 @@ class Blueprint:
             max(self.ore_ore, self.clay_ore, self.geode_ore), self.obsidian_clay, self.geode_obsidian
         )
 
+    def solve(self) -> int:
+        heap = [((0, 0, 0, 0), State())]
+        best = 0
+        touched = set()
+        flow = {}
+        best_flow = State()
 
-@attrs.define
+        while heap:
+            _, old_state = heapq.heappop(heap)
+            for opt in old_state.get_options(self):
+                new_state = old_state.advance(self, opt)
+                if new_state in touched:
+                    continue
+                touched.add(new_state)
+                flow[new_state] = (old_state, opt)
+                new_potential = new_state.potential
+                if new_potential < best:
+                    continue
+                if new_state.score > best:
+                    best = new_state.score
+                    print(f"Blueprint {self.index}, best geodes: {best}, heap: {len(heap)}")
+                    continue
+                if new_state.time_left:
+                    heapq.heappush(
+                        heap,
+                        (
+                            (
+                                -new_state.resources.geode,
+                                -new_state.resources.obsidian,
+                                -new_state.resources.clay,
+                                new_state.time_left,
+                            ),
+                            new_state,
+                        ),
+                    )
+                elif new_state.resources.geode == best:
+                    best_flow = new_state
+
+        history = ""
+        while best_flow in flow:
+            best_flow, opt = flow[best_flow]
+            history += "RCBG"[opt] if opt is not None else "-"
+        print(f"Blueprint {self.index}: {best}: {history[::-1]}")
+        return best
+
+
+@attrs.define(frozen=True, order=True)
 class State:
     resources: Resources = Resources()
     bots: Resources = Resources(ore=1)
-    time_left: int = 24
+    time_left: int = 32
     snubbed: frozenset[int] = frozenset()
-    history: str = ""
 
-    def get_options(self, bp: Blueprint) -> set[int]:
-        options: set[int] = set()
+    def get_options(self, bp: Blueprint) -> Sequence[None | int]:
+        if self.time_left <= 1:
+            return ()
+        options: list[None | int] = [None]
         if self.resources.ore >= bp.ore_ore and self.bots.ore < bp.max_bots.ore:
-            options.add(ORE)
+            options.append(ORE)
         if self.resources.ore >= bp.clay_ore and self.bots.clay < bp.max_bots.clay:
-            options.add(CLAY)
+            options.append(CLAY)
         if (
             self.resources.ore >= bp.obsidian_ore
             and self.resources.clay >= bp.obsidian_clay
             and self.bots.obsidian < bp.max_bots.obsidian
         ):
-            options.add(OBSIDIAN)
+            options.append(OBSIDIAN)
         if self.resources.ore >= bp.geode_ore and self.resources.obsidian >= bp.geode_obsidian:
-            options.add(GEODE)
-        return options - self.snubbed
+            options.append(GEODE)
+        return options[::-1]  # - self.snubbed
+
+    @property
+    def potential(self) -> int:
+        result = self.resources.geode
+        bots = self.bots.geode
+        for i in range(self.time_left):
+            result += bots
+            bots += 1
+        return result
+
+    @property
+    def score(self) -> int:
+        return self.resources.geode + self.time_left * self.bots.geode
 
     def advance(self, bp: Blueprint, build: None | int) -> State:
+        assert self.time_left > 0
         snubbed = self.snubbed
         resources = Resources(*(a + b for a, b in zip(self.resources, self.bots)))
         bots = self.bots
         if build is None:
-            snubbed |= self.get_options(bp)
-            history = self.history + "-"
+            pass  # snubbed |= self.get_options(bp)
         elif build == 0:
             resources = resources._replace(ore=resources.ore - bp.ore_ore)
             bots = bots._replace(ore=bots.ore + 1)
             snubbed = frozenset()
-            history = self.history + "R"
         elif build == 1:
             resources = resources._replace(ore=resources.ore - bp.clay_ore)
             bots = bots._replace(clay=bots.clay + 1)
             snubbed = frozenset()
-            history = self.history + "C"
         elif build == 2:
-            resources = resources._replace(ore=resources.ore - bp.obsidian_ore, clay=resources.clay - bp.obsidian_clay)
+            resources = resources._replace(
+                ore=resources.ore - bp.obsidian_ore,
+                clay=resources.clay - bp.obsidian_clay,
+            )
             bots = bots._replace(obsidian=bots.obsidian + 1)
             snubbed = frozenset()
-            history = self.history + "B"
         elif build == 3:
             resources = resources._replace(
-                clay=resources.clay - bp.obsidian_clay, obsidian=resources.obsidian - bp.geode_obsidian
+                ore=resources.ore - bp.geode_ore,
+                obsidian=resources.obsidian - bp.geode_obsidian,
             )
             bots = bots._replace(geode=bots.geode + 1)
             snubbed = frozenset()
-            history = self.history + "G"
-        return State(resources, bots, self.time_left - 1, snubbed, history)
-
-    def solve(self, bp: Blueprint) -> State:
-        if self.time_left == 1:
-            return self.advance(bp, None)
-        options = self.get_options(bp)
-        states = [self.advance(bp, opt).solve(bp) for opt in (*options, None)]
-        return max(states, key=lambda s: s.resources.geode)
+        else:
+            assert False
+        return State(resources, bots, self.time_left - 1, snubbed)
 
 
-def run(line: str) -> int:
-    bp = Blueprint(line)
-    solved = State().solve(bp)
-    print(f"Blueprint{bp.index:3d}:{solved.resources.geode:3d}, {solved.history}")
-    return solved.resources.geode * bp.index
+def run(bp: Blueprint) -> int:
+    return bp.solve()
 
 
 def main(input: str) -> (int | str | None):
     segments = input.rstrip("\n").split("\n\n")
+    blueprints = [Blueprint(line) for line in segments[0].split("\n")]
 
-    return sum(pool.map(run, segments[0].split("\n"), 1))
+    # return sum(map(run, blueprints[:3]))
+    return reduce_multiply(pool.map(run, blueprints[:3], 1))  # type: ignore[return-value]
 
 
-EXPECTED = 33
 if __name__ == "__main__":
-    pool = multiprocessing.pool.Pool()
+    pool = multiprocessing.pool.Pool(3)
     THIS_DIR = Path(__file__).parent
     EXAMPLE_FILE = THIS_DIR / "example.input"
     INPUT_FILE = THIS_DIR / "input.input"
     if not INPUT_FILE.exists():
         INPUT_FILE.write_text(aocd.data, encoding="utf8")
-    result = main(EXAMPLE_FILE.read_text(encoding="ansi"))
-    if result != EXPECTED:
-        print(f"Expected {EXPECTED!r} but got {result!r} instead!")
-        raise SystemExit()
-    else:
-        print("Example passed.")
     aocd.submit(main(INPUT_FILE.read_text(encoding="ansi")))
