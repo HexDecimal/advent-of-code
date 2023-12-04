2335d95a2394acb70dc0feec1a084cd8763d14dc
Kyle Benesch <4b796c65+github@gmail.com>
2023-12-03 21:33:12 -0800

    2023-04

--- 2023/04/2023-04.py
+++ 2023/04/2023-04.py
@@ -1,30 +1,37 @@
 from __future__ import annotations
 
-import functools
-import itertools
-import operator
-import re
 from pathlib import Path
 from typing import *  # noqa: F403
 
 import aocd  # type: ignore
-import numpy as np
-from aoc import as_array, as_bool_array, as_ord_array, ints, reduce_multiply, split_ints
-from numpy.typing import NDArray
 from parse import parse  # type: ignore[import-untyped]
 
 
 def main(input: str) -> int | str | None:
     segments = input.rstrip("\n").split("\n\n")
-    # array = as_array(segments[0]).astype(np.int32)
-    # numbers = ints(segments[0].split(","))
     lines = segments[0].split("\n")
+    last_card, _ = parse("{:d}{}", lines[-1].removeprefix("Card").strip())
+    total = 0
+    my_cards = Counter[int]({i: 1 for i in range(1, last_card + 1)})
     for line in lines:
-        pass
-    return None
+        left: str
+        right: str
+        line = line.removeprefix("Card").strip()  # noqa: PLW2901
+        print(line)
+        card_n, left, right = parse("{:d}: {}|{}", line)
+        left_n = {int(s) for s in left.strip().split(" ") if s}
+        right_n = {int(s) for s in right.strip().split(" ") if s}
+        matches = len(left_n & right_n)
+        print(range(card_n + 1, card_n + 1 + matches))
+        for i in range(card_n + 1, card_n + 1 + matches):
+            my_cards[i] += my_cards[card_n]
+        total += my_cards[card_n]
+        print(f"CARD: {card_n} {my_cards[card_n]=} {matches=} {total=}")
+
+    return total
 
 
-EXPECTED = 0
+EXPECTED = 30
 if __name__ == "__main__":
     THIS_DIR = Path(__file__).parent
     EXAMPLE_FILE = THIS_DIR / "example.input"
--- 2023/04/2023-04a.py
+++ 2023/04/2023-04a.py
@@ -1,30 +1,33 @@
 from __future__ import annotations
 
-import functools
-import itertools
-import operator
-import re
 from pathlib import Path
 from typing import *  # noqa: F403
 
 import aocd  # type: ignore
-import numpy as np
-from aoc import as_array, as_bool_array, as_ord_array, ints, reduce_multiply, split_ints
-from numpy.typing import NDArray
 from parse import parse  # type: ignore[import-untyped]
 
 
 def main(input: str) -> int | str | None:
     segments = input.rstrip("\n").split("\n\n")
-    # array = as_array(segments[0]).astype(np.int32)
-    # numbers = ints(segments[0].split(","))
     lines = segments[0].split("\n")
+    total = 0
     for line in lines:
-        pass
-    return None
+        left: str
+        right: str
+        line = line.removeprefix("Card").strip()  # noqa: PLW2901
+        print(line)
+        card_n, left, right = parse("{:d}: {}|{}", line)
+        left_n = {int(s) for s in left.strip().split(" ") if s}
+        right_n = {int(s) for s in right.strip().split(" ") if s}
+        matches = len(left_n & right_n)
+        score = 2 ** (matches - 1) if matches else 0
+        total += score
+        print(f"CARD: {card_n} {left_n & right_n} {score=}")
+
+    return total
 
 
-EXPECTED = 0
+EXPECTED = 13
 if __name__ == "__main__":
     THIS_DIR = Path(__file__).parent
     EXAMPLE_FILE = THIS_DIR / "example.input"
